e58fa1f9fdc57f7587227d534abc9b9d02c5570a
jarbasal <jarbasai@mailfence.com>
2020-02-01 19:16:17 +0000

    0.5

--- cli_satellite/__init__.py
+++ cli_satellite/__init__.py
@@ -1,27 +1,15 @@
 import json
-import logging
-import sys
 from threading import Thread
-
-from autobahn.twisted.websocket import WebSocketClientFactory, \
-    WebSocketClientProtocol
-from twisted.internet.protocol import ReconnectingClientFactory
+from jarbas_hive_mind.slave.terminal import HiveMindTerminal, HiveMindTerminalProtocol
 
 platform = "JarbasCliTerminalv0.1"
-logger = logging.getLogger(platform)
-logger.addHandler(logging.StreamHandler(sys.stdout))
-logger.setLevel("INFO")
+
+from jarbas_utils.log import LOG
 
 
-class JarbasCliTerminalProtocol(WebSocketClientProtocol):
-
-    def onConnect(self, response):
-        logger.info("[INFO] Server connected: {0}".format(response.peer))
-        self.factory.client = self
-        self.factory.status = "connected"
-
+class JarbasCliTerminalProtocol(HiveMindTerminalProtocol):
     def onOpen(self):
-        logger.info("[INFO] WebSocket connection open. ")
+        LOG.info("[INFO] WebSocket connection open. ")
         self.input_loop = Thread(target=self.get_cli_input)
         self.input_loop.setDaemon(True)
         self.input_loop.start()
@@ -32,23 +20,15 @@ class JarbasCliTerminalProtocol(WebSocketClientProtocol):
             msg = json.loads(payload)
             if msg.get("type", "") == "speak":
                 utterance = msg["data"]["utterance"]
-                logger.info("[OUTPUT] " + utterance)
+                LOG.info("[OUTPUT] " + utterance)
             elif msg.get("type", "") == "hive.complete_intent_failure":
-                logger.error("[ERROR] complete intent failure")
+                LOG.error("[ERROR] complete intent failure")
         else:
             pass
 
-    def onClose(self, wasClean, code, reason):
-        logger.info("[INFO] WebSocket connection closed: {0}".format(reason))
-        self.factory.client = None
-        self.factory.status = "disconnected"
-        if "Internalservererror:InvalidAPIkey" in reason:
-            logger.error("[ERROR] invalid user:key provided")
-            raise ConnectionAbortedError("invalid user:key provided")
-
     # cli input thread
     def get_cli_input(self):
-        logger.info("[INFO] waiting for input")
+        LOG.info("[INFO] waiting for input")
         while True:
             line = input("")
             msg = {"data": {"utterances": [line], "lang": "en-us"},
@@ -59,26 +39,4 @@ class JarbasCliTerminalProtocol(WebSocketClientProtocol):
             msg = json.dumps(msg)
             msg = bytes(msg, encoding="utf-8")
             self.sendMessage(msg, False)
-
-
-class JarbasCliTerminal(WebSocketClientFactory, ReconnectingClientFactory):
-    protocol = JarbasCliTerminalProtocol
-
-    def __init__(self, *args, **kwargs):
-        super(JarbasCliTerminal, self).__init__(*args, **kwargs)
-        self.status = "disconnected"
-        self.client = None
-
-    # websocket handlers
-    def clientConnectionFailed(self, connector, reason):
-        logger.info("[INFO] Client connection failed: " + str(
-            reason) + " .. retrying ..")
-        self.status = "disconnected"
-        self.retry(connector)
-
-    def clientConnectionLost(self, connector, reason):
-        logger.info("[INFO] Client connection lost: " + str(
-            reason) + " .. retrying ..")
-        self.status = "disconnected"
-        self.retry(connector)
 
--- cli_satellite/__main__.py
+++ cli_satellite/__main__.py
@@ -1,22 +1,19 @@
-import base64
-from cli_satellite import JarbasCliTerminal, platform, logger
-from twisted.internet import reactor, ssl
+from jarbas_hive_mind.slave.terminal import HiveMindTerminal
+from cli_satellite import JarbasCliTerminalProtocol, platform
+from jarbas_hive_mind import HiveMindConnection
 
 
 def connect_to_hivemind(host="127.0.0.1",
-                        port=5678, name="Jarbas Cli Terminal",
-                        api="cli_key", useragent=platform):
-    authorization = bytes(name + ":" + api, encoding="utf-8")
-    usernamePasswordDecoded = authorization
-    api = base64.b64encode(usernamePasswordDecoded)
-    headers = {'authorization': api}
-    address = u"wss://" + host + u":" + str(port)
-    logger.info("[INFO] connecting to hive mind at " + address)
-    terminal = JarbasCliTerminal(address, headers=headers,
-                                 useragent=useragent)
-    contextFactory = ssl.ClientContextFactory()
-    reactor.connectSSL(host, port, terminal, contextFactory)
-    reactor.run()
+                        port=5678, name="JarbasCliTerminal",
+                        key="cli_key", useragent=platform):
+    con = HiveMindConnection(host, port)
+
+    terminal = HiveMindTerminal(con.address,
+                                headers=con.get_headers(name, key),
+                                useragent=useragent)
+    terminal.protocol = JarbasCliTerminalProtocol
+
+    con.secure_connect(terminal)
 
 
 if __name__ == '__main__':
