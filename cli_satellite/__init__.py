7e49fb71935e330cb673a6cac702a394f8472a9d
jarbasal <jarbasai@mailfence.com>
2020-02-02 09:08:06 +0000

    add colors

--- cli_satellite/__init__.py
+++ cli_satellite/__init__.py
@@ -1,12 +1,15 @@
 import json
 from threading import Thread
+from time import sleep
 from jarbas_hive_mind.slave.terminal import HiveMindTerminalProtocol, HiveMindTerminal
 from jarbas_utils.log import LOG
 
-platform = "JarbasCliTerminalv0.1"
+platform = "JarbasCliTerminalV0.2"
 
 
 class JarbasCliTerminalProtocol(HiveMindTerminalProtocol):
+    waiting = False
+
     def onOpen(self):
         LOG.info("WebSocket connection open. ")
         self.input_loop = Thread(target=self.get_cli_input)
@@ -19,17 +22,25 @@ class JarbasCliTerminalProtocol(HiveMindTerminalProtocol):
             msg = json.loads(payload)
             if msg.get("type", "") == "speak":
                 utterance = msg["data"]["utterance"]
-                LOG.info("[OUTPUT] " + utterance)
+                print('\x1b[6;34;40m MYCROFT: ' + utterance + ' \x1b[0m')
+                LOG.info("[SPEAK] " + utterance)
             elif msg.get("type", "") == "hive.complete_intent_failure":
                 LOG.error("complete intent failure")
+            elif msg.get("type", "") == "mycroft.skill.handler.complete":
+                LOG.debug("Request handled")
+                self.waiting = False
         else:
             pass
 
     # cli input thread
     def get_cli_input(self):
-        LOG.info("waiting for input")
         while True:
-            line = input("")
+            if self.waiting:
+                sleep(0.3)
+                continue
+            LOG.info("waiting for input")
+            line = input("\x1b[6;33;40m INPUT: \x1b[0m")
+            LOG.info("[UTTERANCE] " + line)
             msg = {"data": {"utterances": [line], "lang": "en-us"},
                    "type": "recognizer_loop:utterance",
                    "context": {"source": self.peer,
@@ -38,6 +49,8 @@ class JarbasCliTerminalProtocol(HiveMindTerminalProtocol):
             msg = json.dumps(msg)
             msg = bytes(msg, encoding="utf-8")
             self.sendMessage(msg, False)
+            self.waiting = True
+            print('\x1b[6;33;40m YOU: ' + line + ' \x1b[0m')
 
 
 class JarbasCliTerminal(HiveMindTerminal):
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='HiveMind-cli',
+    version='0.2',
+    packages=['cli_satellite'],
+    url='https://github.com/OpenJarbas/HiveMind-cli',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='Mycroft Remote Cli'
+)
