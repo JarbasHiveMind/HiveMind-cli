777d00a24c1ee0b6fffb1d602f5fd260067b821f
jarbasal <jarbasai@mailfence.com>
2020-06-10 00:18:32 +0100

    0.4

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
     description='Mycroft Remote Cli',
+    install_requires=["jarbas_hive_mind>=0.10.1"],
     entry_points={
         'console_scripts': [
             'HiveMind-cli=cli_satellite.__main__:main'
