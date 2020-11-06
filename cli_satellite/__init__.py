77c75885dda766acf13c156222ba817353822f8b
jarbasal <jarbasai@mailfence.com>
2020-11-06 05:21:12 +0000

    debug

--- cli_satellite/__init__.py
+++ cli_satellite/__init__.py
@@ -2,8 +2,6 @@ from jarbas_hive_mind.slave.terminal import HiveMindTerminalProtocol, HiveMindTe
 from jarbas_utils.log import LOG
 from jarbas_utils import create_daemon
 from jarbas_utils.messagebus import Message
-from curses import wrapper
-import curses
 from time import sleep
 
 
@@ -14,7 +12,7 @@ class JarbasCliTerminalProtocol(HiveMindTerminalProtocol):
 
     def onOpen(self):
         super().onOpen()
-        create_daemon(self.factory.run_gui)
+        create_daemon(self.factory.run_cli)
 
 
 class JarbasCliTerminal(HiveMindTerminal):
@@ -26,49 +24,26 @@ class JarbasCliTerminal(HiveMindTerminal):
 
     # terminal
     def speak(self, utterance):
-        self.msg_box.addstr("Mycroft > {utterance} \n".format(utterance=utterance))
+        print("Mycroft:", utterance)
 
     def on_handled(self):
+        print("DEBUG: Intent handling finished")
         self.waiting = False
 
-    def _run_curses_gui(self, stdscr):
-        curses.echo()
-        lasty, lastx = stdscr.getmaxyx()
-        self.input_box = curses.newwin(0, lastx, lasty - 3, 0)
-
-        self.header_box = curses.newwin(lasty - 4, lastx, 0, 0)
-        self.header_box.addstr("=== {platform} ===".format(platform=platform))
-
-        self.msg_box = curses.newwin(lasty - 4, lastx, 1, 0)
-        self.msg_box.scrollok(True)
-
-        def refresh():
-            while True:
-                self.header_box.refresh()
-                self.msg_box.refresh()
-                self.input_box.refresh()
-                sleep(0.5)
-
-        create_daemon(refresh)
-
+    def run_cli(self):
         while True:
-            self.input_box.addstr("Input > ")
-            msg = self.input_box.getstr()
-            if msg:
-                utterance = str(msg.decode("utf-8"))
-                self.msg_box.addstr("You > " + utterance + "\n")
-                msg = {"data": {"utterances": [utterance],
-                                "lang": "en-us"},
-                       "type": "recognizer_loop:utterance",
-                       "context": {"source": self.client.peer,
-                                   "destination": "hive_mind",
-                                   "platform": platform}}
-                self.send_to_hivemind_bus(msg)
-                self.waiting = True
-            self.input_box.clear()
-
-    def run_gui(self):
-        wrapper(self._run_curses_gui)
+            utterance = input("Input:")
+            msg = {"data": {"utterances": [utterance],
+                            "lang": "en-us"},
+                   "type": "recognizer_loop:utterance",
+                   "context": {"source": self.client.peer,
+                               "destination": "hive_mind",
+                               "platform": platform}}
+            self.send_to_hivemind_bus(msg)
+            self.waiting = True
+            print("DEBUG: Waiting for Intent handling, BLOCKING HERE")
+            while self.waiting:
+                sleep(0.5)
 
     # parsed protocol messages
     def handle_incoming_mycroft(self, message):
