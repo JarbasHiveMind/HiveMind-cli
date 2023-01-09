7c7760d9a5ef0aa11636e4d84e026256b77971cf
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-01-09 21:01:16 +0000

    refactor/hivemind-bus-client (#11)
    * refactor/hivemind-bus-client
    
    minimal dependencies implementation
    
    * autodiscovery + launcher args
    
    * accept self-signed cli arg
    
    * xdg
    
    * Revert "xdg"
    
    This reverts commit 217f0203052d7d994997e0a006e45a260afb3e35.
    
    * self signed arg

---
+++ hivemind_cli_terminal/__main__.py
@@ -0,0 +1,73 @@
+from HiveMind_presence import LocalDiscovery
+from hivemind_cli_terminal import JarbasCliTerminal, JarbasCursesTerminal
+
+
+def connect_to_hivemind(access_key=None,
+                        host="wss://127.0.0.1",
+                        port=5678,
+                        crypto_key=None,
+                        curses=False,
+                        self_signed=False,
+                        bus=None):
+    if curses:
+        terminal = JarbasCursesTerminal(access_key,
+                                        host=host,
+                                        port=port,
+                                        self_signed=self_signed,
+                                        crypto_key=crypto_key,
+                                        bus=bus)
+    else:
+        terminal = JarbasCliTerminal(access_key,
+                                     host=host,
+                                     port=port,
+                                     self_signed=self_signed,
+                                     crypto_key=crypto_key,
+                                     bus=bus)
+    # this is a thread, can be set as a daemon
+    terminal.start()
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--access-key", help="access key", required=True)
+    parser.add_argument("--crypto-key", help="payload encryption key", default=None)
+    parser.add_argument("--host", help="HiveMind host")
+    parser.add_argument("--port", help="HiveMind port number (default 5678)", default=5678)
+    parser.add_argument("--no-curses", help="do not use curses interface", action="store_true")
+    parser.add_argument("--self-signed", help="accept self signed ssl certificates", action="store_true")
+
+    args = parser.parse_args()
+
+    if not args.host:
+        print("You did not specify a host to connect")
+        scan = input("scan for node and attempt to connect? y/n: ")
+        if not scan.lower().startswith("y"):
+            print("Scan aborted and host not specified, exiting")
+            exit(2)
+        scanner = LocalDiscovery()
+        for node in scanner.scan():
+            print("Found HiveMind node: ", node.address)
+            try:
+                bus = node.connect(args.access_key, args.crypto_key,
+                                   useragent=JarbasCliTerminal.platform,
+                                   self_signed=args.self_signed)
+                scanner.stop()
+                connect_to_hivemind(curses=not args.no_curses, bus=bus)
+            except:
+                print("failed to connect!")
+        exit(2)
+    elif not args.host.startswith("ws"):
+        print("Invalid host, please specify a protocol")
+        print(f"ws://{args.host} or wss://{args.host}")
+        exit(1)
+    connect_to_hivemind(host=args.host,
+                        port=args.port,
+                        access_key=args.access_key,
+                        crypto_key=args.crypto_key,
+                        self_signed=args.self_signed,
+                        curses=not args.no_curses)
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='HiveMind-cli',
-    version='0.4.4',
-    packages=['cli_satellite'],
+    version='0.5.0a1',
+    packages=['hivemind_cli_terminal'],
     url='https://github.com/OpenJarbas/HiveMind-cli',
     license='MIT',
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
     description='Mycroft Remote Cli',
-    install_requires=["jarbas_hive_mind>=0.10.7"],
+    install_requires=["hivemind_bus_client~=0.0.3a2",
+                      "HiveMind_presence~=0.0.2a1"],
     entry_points={
         'console_scripts': [
             'HiveMind-cli=cli_satellite.__main__:main'
