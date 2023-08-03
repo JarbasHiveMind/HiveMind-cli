4d00946f98e1765fa92936ce043dce9d81b57a4d
JarbasAi <jarbasai@mailfence.com>
2023-08-03 18:05:08 +0100

    automations

--- setup.py
+++ setup.py
@@ -1,16 +1,56 @@
+import os
 from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'hivemind_cli_terminal', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha:
+        version += f"a{alpha}"
+    return version
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
 
 setup(
     name='HiveMind-cli',
-    version='0.5.0a1',
+    version=get_version(),
     packages=['hivemind_cli_terminal'],
     url='https://github.com/OpenJarbas/HiveMind-cli',
     license='MIT',
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
     description='Mycroft Remote Cli',
-    install_requires=["hivemind_bus_client~=0.0.3a2",
-                      "HiveMind_presence~=0.0.2a1"],
+    install_requires=required("requirements.txt"),
     entry_points={
         'console_scripts': [
             'hivemind-cli=hivemind_cli_terminal.__main__:main'
